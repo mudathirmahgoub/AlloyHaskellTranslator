d10f6c77f72eaabe02ebfa12f8f1771c1a124a7d
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-12-17 22:23:35 -0600

    changes

--- app/Main.hs
+++ app/Main.hs
@@ -4,20 +4,20 @@ import           Alloy
 import           Translator
 
 a = Prim { isAbstract       = False
-            , children         = []
-            , parent           = Univ
-            , primLabel        = "A"
-            , primMultiplicity = One
-            , primFacts        = []
-            }
+         , children         = []
+         , parent           = Univ
+         , primLabel        = "A"
+         , primMultiplicity = One
+         , primFacts        = []
+         }
 
 b = Prim { isAbstract       = False
-            , children         = []
-            , parent           = Univ
-            , primLabel        = "B"
-            , primMultiplicity = One
-            , primFacts        = []
-            }
+         , children         = []
+         , parent           = Univ
+         , primLabel        = "B"
+         , primMultiplicity = One
+         , primFacts        = []
+         }
 
 printTranslation :: IO ()
 printTranslation = print (translate (AlloyBinary PLUS (Prime a) (Prime b)))
