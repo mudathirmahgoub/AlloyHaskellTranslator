aaf30a1820478138c99ef1eab90ec062d7d4d434
mudathirmahgoub <mudathirmahgoub@gmail.com>
2020-01-03 22:06:13 -0600

    changes

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,17 @@
+-- test/LibTests.hs
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  defaultMain (testGroup "Project tests" [test1, test2])
+
+test1 :: TestTree
+test1 = testCase "test1"
+  (assertEqual "test1" "Hello world" "Hello world")
+
+test2 :: TestTree
+test2 = testCase "test2"
+  (assertEqual "10 = 5 + 5" 10 (5 + 5))
